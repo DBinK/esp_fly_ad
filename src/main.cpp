16624488a28dd49a43628f494012c7ed90dff789
DBinK <DBinKv1@Gmail.com>
2025-01-18 15:49:26 +0800

    feat(imu): 添加 SensorFusion 库并实现数据融合
    - 在 platformio.ini 中添加 SensorFusion 库依赖
    - 在 main.cpp 中引入 SensorFusion 库并进行数据融合处理
    - 新增变量用于存储融合后的姿态数据
    - 在 loop 中调用融合算法并输出结果

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "imu.h"
+#include "SensorFusion.h" //SF
 
 IMUClass imu;  // 使用自定义引脚 IMUClass imu(10, 11, 12, 13);
+SF fusion;
 
+float gx, gy, gz, ax, ay, az, temp;
+float pitch, roll, yaw;
+float deltat;
 
 void setup() {
     Serial.begin(115200);
@@ -19,11 +24,21 @@ void setup() {
 
 
 void loop() {
-    float accX, accY, accZ, gyrX, gyrY, gyrZ, temp;
-    imu.getData(accX, accY, accZ, gyrX, gyrY, gyrZ, temp);
+    imu.getData(ax, ay, az, gx, gy, gz, temp);
     
     // 显示数据
-    Serial.printf("AccX: %f\tAccY: %f\tAccZ: %f\tGyrX: %f\tGyrY: %f\tGyrZ: %f\tTemp: %f\n", 
-                  accX, accY, accZ, gyrX, gyrY, gyrZ, temp);
+    Serial.printf("AccX: %f\t AccY: %f\t AccZ: %f\t GyrX: %f\t GyrY: %f\t GyrZ: %f\t Temp: %f\n", 
+                  ax, ay, az, gx, gy, gz, temp);
+
+    deltat = fusion.deltatUpdate(); //this have to be done before calling the fusion update
+    //choose only one of these two:
+    fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, deltat);  //mahony is suggested if there isn't the mag and the mcu is slow
+
+    pitch = fusion.getPitch();
+    roll = fusion.getRoll();    //you could also use getRollRadians() ecc
+    yaw = fusion.getYaw();
+
+    Serial.printf("Pitch: %.2f\t Roll: %.2f\t Yaw: %.2f \n", pitch, roll, yaw);
+
     delay(100);
 }
