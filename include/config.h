bfd7384778ceab420ca06a8303d3ba2863471def
DBinK <DBinKv1@Gmail.com>
2025-01-19 23:02:58 +0800

    feat(control): 实现基础 PID 控制并添加 ESP-NOW 支持
    - 在 config.h 中更新了 PID 参数，默认设置为 1
    - 新增 OutRate 结构体用于表示三维向量
    - 添加了 ESPNowReceiver 对象以支持 ESP-NOW 通信
    - 重构了 main 函数，实现了基础的 PID 控制逻辑
    - 优化了数据处理和电机控制算法

--- include/config.h
+++ include/config.h
@@ -4,28 +4,28 @@
 #include <Arduino.h>
 
 // 角度环数据PID参数
-float ROL_ANGLE_P = 0;
+float ROL_ANGLE_P = 1;
 float ROL_ANGLE_I = 0;
 float ROL_ANGLE_D = 0;
 
-float PIT_ANGLE_P = 0;
+float PIT_ANGLE_P = 1;
 float PIT_ANGLE_I = 0;
 float PIT_ANGLE_D = 0;
 
-float YAW_ANGLE_P = 0;
+float YAW_ANGLE_P = 1;
 float YAW_ANGLE_I = 0;
 float YAW_ANGLE_D = 0;
 
 // 角速度环数据PID参数
-float ROL_RATE_P = 0;
+float ROL_RATE_P = 1;
 float ROL_RATE_I = 0;
 float ROL_RATE_D = 0;
 
-float PIT_RATE_P = 0;
+float PIT_RATE_P = 1;
 float PIT_RATE_I = 0;
 float PIT_RATE_D = 0;
 
-float YAW_RATE_P = 0;
+float YAW_RATE_P = 1;
 float YAW_RATE_I = 0;
 float YAW_RATE_D = 0;
 
@@ -54,5 +54,10 @@ typedef struct {
     float yaw;    // gz 逆时针为负
 } MeasureRate;    // 测量角速度
 
+typedef struct {
+    float roll;   // gx
+    float pitch;  // gy
+    float yaw;    // gz
+} OutRate;      // 三维向量
 
 #endif // CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -12,90 +12,138 @@ TargetAngle tgAngle;
 MeasureAngle msAngle;
 TargetRate tgRate;
 MeasureRate msRate;
+OutRate outRate;
 
 // 全局变量
 float deltat;
 
 // 创建IMU对象
-IMUClass imu;  // 使用自定义引脚 IMUClass imu(10, 11, 12, 13);
+IMUClass imu; // 使用自定义引脚 IMUClass imu(10, 11, 12, 13);
+
+// 创建NOW对象
+static uint8_t Mac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // 默认广播地址
+ESPNowReceiver receiver(Mac); 
 
 // 创建电机对象
 QuadMotorController motors; // 使用自定义引脚 QuadMotorController motors(9, 8, 7, 6);
 
-// 创建PID对象, 参数在 config.h 中定义
-QuickPID pidRollAngle(&msAngle.roll, &tgAngle.roll, &tgRate.roll, 
-                        ROL_ANGLE_P, ROL_ANGLE_I, ROL_ANGLE_D, QuickPID::Action::direct);
-QuickPID pidPitchAngle(&msAngle.pitch, &tgAngle.pitch, &tgRate.pitch, 
-                        PIT_ANGLE_P, PIT_ANGLE_I, PIT_ANGLE_D, QuickPID::Action::direct);
-// QuickPID pidYawAngle(&msAngle.yaw, &tgAngle.yaw, &tgRate.yaw, 
+// 创建PID对象, 参数在 config.h 中定义, QuickPID(float *Input, float *Output, float *Setpoint);
+QuickPID pidRollAngle(&msAngle.roll, &tgAngle.roll, &tgRate.roll,
+                      ROL_ANGLE_P, ROL_ANGLE_I, ROL_ANGLE_D, QuickPID::Action::direct);
+QuickPID pidPitchAngle(&msAngle.pitch, &tgAngle.pitch, &tgRate.pitch,
+                       PIT_ANGLE_P, PIT_ANGLE_I, PIT_ANGLE_D, QuickPID::Action::direct);
+// QuickPID pidYawAngle(&msAngle.yaw, &tgAngle.yaw, &tgRate.yaw,
 //                         YAW_ANGLE_P, YAW_ANGLE_I, YAW_ANGLE_D, QuickPID::Action::direct);
 
-QuickPID pidRollRate(&msRate.roll, &tgRate.roll, &tgRate.roll, 
-                        ROL_RATE_P, ROL_RATE_I, ROL_RATE_D, QuickPID::Action::direct);
-QuickPID pidPitchRate(&msRate.pitch, &tgRate.pitch, &tgRate.pitch, 
-                        PIT_RATE_P, PIT_RATE_I, PIT_RATE_D, QuickPID::Action::direct);
-QuickPID pidYawRate(&msRate.yaw, &tgRate.yaw, &tgRate.yaw, YAW_RATE_P, 
-                        YAW_RATE_I, YAW_RATE_D, QuickPID::Action::direct);
+QuickPID pidRollRate(&msRate.roll, &tgRate.roll, &outRate.roll,
+                     ROL_RATE_P, ROL_RATE_I, ROL_RATE_D, QuickPID::Action::direct);
+QuickPID pidPitchRate(&msRate.pitch, &tgRate.pitch, &outRate.pitch,
+                      PIT_RATE_P, PIT_RATE_I, PIT_RATE_D, QuickPID::Action::direct);
+QuickPID pidYawRate(&msRate.yaw, &tgRate.yaw, &outRate.yaw,
+                    YAW_RATE_P, YAW_RATE_I, YAW_RATE_D, QuickPID::Action::direct);
 
 LowPassFilter lpf(0.1);
 
-void setup() {
+void setup()
+{
     // 初始化串口
     Serial.begin(115200);
-    while (!Serial) {}
-    Serial.println("初始化串口成功...");
-    
-    // 初始化IMU
-    if (!imu.begin()) {
-        Serial.println("IMU initialization unsuccessful");
-        while (1) {}
+    while (!Serial)
+    {
     }
-    imu.calculateGyrBias(500, 3);  // 计算陀螺仪Z轴零偏
+    Serial.println("初始化串口成功...");
+
+    // 初始化IMU
+    if (!imu.begin())
+    {
+        Serial.println("IMU initialization unsuccessful");
+        while (1)
+        {
+        }
+    }
+    imu.calculateGyrBias(500, 3); // 计算陀螺仪Z轴零偏
     Serial.println("初始化 ICM4268 成功...");
 
     // 初始化PID
     pidRollAngle.SetTunings(ROL_ANGLE_P, ROL_ANGLE_I, ROL_ANGLE_D);
-    pidRollAngle.SetMode(1);  // 设置PID模式为自动控制
+    pidRollAngle.SetMode(1); // 设置PID模式为自动控制
     pidPitchAngle.SetTunings(PIT_ANGLE_P, PIT_ANGLE_I, PIT_ANGLE_D);
-    pidPitchAngle.SetMode(1);  
+    pidPitchAngle.SetMode(1);
     // pidYawAngle.SetTunings(YAW_ANGLE_P, YAW_ANGLE_I, YAW_ANGLE_D);
-    // pidYawAngle.SetMode(1);  
-    
+    // pidYawAngle.SetMode(1);
+
     pidRollRate.SetTunings(ROL_RATE_P, ROL_RATE_I, ROL_RATE_D);
-    pidRollRate.SetMode(1);  
+    pidRollRate.SetMode(1);
     pidPitchRate.SetTunings(PIT_RATE_P, PIT_RATE_I, PIT_RATE_D);
-    pidPitchRate.SetMode(1);  
+    pidPitchRate.SetMode(1);
     pidYawRate.SetTunings(YAW_RATE_P, YAW_RATE_I, YAW_RATE_D);
-    pidYawRate.SetMode(1);  
+    pidYawRate.SetMode(1);
 
     // 初始化电机
     motors.reset(); // 重置电机
 }
 
+static TickType_t xLastWakeTime = 0; // 用于 vTaskDelayUntil 的变量
+void loop()
+{
+    int* parsedData = receiver.getParsedData();
+    int parsedDataSize = receiver.getParsedDataSize();
+    const uint8_t* lastMacAddr = receiver.getLastMacAddr();
 
-void loop() {
+    // 打印解析后的数据
+    Serial.print("now Data: ");
+    for (int i = 0; i < parsedDataSize; i++) {
+        Serial.print(parsedData[i]);
+        Serial.print(" ");
+    }
+    Serial.println();
 
     imu.update();
-    
+
     // 显示数据
-    // Serial.printf("AccX: %f\t AccY: %f\t AccZ: %f\t GyrX: %f\t GyrY: %f\t GyrZ: %f\t Temp: %f\n", 
+    // Serial.printf("AccX: %f\t AccY: %f\t AccZ: %f\t GyrX: %f\t GyrY: %f\t GyrZ: %f\t Temp: %f\n",
     //              ax, ay, az, gx, gy, gz, temp);
 
-    imu.getPitchRollYaw(msAngle.pitch, msAngle.roll, msAngle.yaw);  // 获取姿态角
-    imu.getGyrData(msRate.pitch, msRate.roll, msRate.yaw);  // 获取角速度
-    deltat = imu.getDeltat() * 1000;        // ms
-    
+    imu.getPitchRollYaw(msAngle.pitch, msAngle.roll, msAngle.yaw); // 获取姿态角
+    imu.getGyrData(msRate.pitch, msRate.roll, msRate.yaw);         // 获取角速度
+    deltat = imu.getDeltat() * 1000;                               // ms
+
     // 对于倒置的IMU，将pitch和roll加减180度, roll 取反
     msAngle.pitch = (msAngle.pitch > 0) ? (msAngle.pitch - 180) : (msAngle.pitch + 180);
-    msAngle.roll  =  -((msAngle.roll > 0) ?  (msAngle.roll - 180) :  (msAngle.roll + 180));
+    msAngle.roll = -((msAngle.roll > 0) ? (msAngle.roll - 180) : (msAngle.roll + 180));
 
     // 低通滤波
-    float filteredRoll = lpf.filter(msAngle.roll);
+    msAngle.pitch = lpf.filter(msAngle.pitch);
+    msAngle.roll = lpf.filter(msAngle.roll);
 
     // Serial.printf("rate: Pitch: %.2f\t Roll: %.2f\t Yaw: %.2f\t deltat: %.2f ms \n", msRate.pitch, msRate.roll, msRate.yaw, deltat);
-    Serial.printf("angle: Pitch: %.2f\t Roll: %.2f\t Yaw: %.2f\t deltat: %.2f ms \n", msAngle.pitch, filteredRoll, msAngle.yaw, deltat);
+    // Serial.printf("angle: Pitch: %.2f\t Roll: %.2f\t Yaw: %.2f\t deltat:%.6f ms \n",
+    //              msAngle.pitch, msAngle.roll, msAngle.yaw, deltat);
 
-    motors.setMotorsThr((int)msAngle.pitch*5, (int)msAngle.roll*5, 10, 10); // 设置电机转速
+    // PID 控制
+    pidRollAngle.Compute();
+    pidPitchAngle.Compute();
+    // pidYawAngle.Compute();
+    pidRollRate.Compute();
+    pidPitchRate.Compute();
+    pidYawRate.Compute();
 
-    delay(10);
+    // 电机控制
+    int rc_thr = 100;
+    int motor1 = rc_thr + outRate.roll - outRate.pitch - outRate.yaw;
+    int motor2 = rc_thr - outRate.roll - outRate.pitch + outRate.yaw;
+    int motor3 = rc_thr - outRate.roll + outRate.pitch - outRate.yaw;
+    int motor4 = rc_thr + outRate.roll + outRate.pitch + outRate.yaw;
+
+    Serial.printf("out: Pitch: %.2f\t Roll: %.2f\t Yaw: %.2f\t deltat: %.2f ms \n", outRate.pitch, outRate.roll, outRate.yaw, deltat);
+    Serial.printf("motors: %d %d %d %d\n", motor1, motor2, motor3, motor4);
+
+    motors.setMotorsThr(motor1, motor2, motor3, motor4); // 设置电机推力
+
+    // 修正 vTaskDelayUntil 的使用
+    if (xLastWakeTime == 0)
+    {
+        xLastWakeTime = xTaskGetTickCount();
+    }
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10)); // 10ms
 }
